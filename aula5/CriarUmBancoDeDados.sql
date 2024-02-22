6cc961e06bf9f490b30d575e0febca4ba2d252db
Carlos Rogério <138127770+0pripyat92@users.noreply.github.com>
2024-02-22 17:53:22 -0300

    commit aula 5

---
+++ aula5/CriarUmBancoDeDados.sql
@@ -0,0 +1,10 @@
+--Curso de Mysql - Criar um Banco de dados - Create database e Drop database - Aula 5
+
+#Criando um banco de dados
+create database test
+
+#Usando função (if not) para verificar a existencia de um banco de dados e que se caso ja exista retornar a não criação do mesmo
+create database if not exists test
+
+#Para eliminar um banco de dados
+drop database test
